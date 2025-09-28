c75ab718ce922622b51a9213ad27fecb3b8ff326
Hector Velasquez <hectordv28@gmail.com>
2025-09-28 13:39:20 -0500

    Agregar archivos de proyecto.

---
+++ Cajero2.0/CuentaBancaria.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Cajero
+{
+    internal class
+
+
+
+
+        CuentaBancaria
+    {
+        public string NumeroCuenta { get; private set; }
+        private string Clave;
+        public double Saldo { get; private set; }
+
+        private List<string> movimientos;
+        private string ArchivoMovimientos;
+        private string ArchivoClave;
+        private string ArchivoSaldo;
+        public string ArchivoCuenta;
+
+
+        public CuentaBancaria(string NumeroCuenta)
+        {
+            NumeroCuenta = NumeroCuenta;
+            ArchivoCuenta = $"C:\\Users\\hecto\\Downloads\\Cajero2.0\\usuario{NumeroCuenta}";// Carpeta para guardar los archivos de cada usuario
+            ArchivoSaldo = Path.Combine($"C:\\Users\\hecto\\Downloads\\Cajero2.0\\usuario{NumeroCuenta}\\Saldo.txt");// Ruta completa del archivo de saldo
+            ArchivoClave = Path.Combine($"C:\\Users\\hecto\\Downloads\\Cajero2.0\\usuario{NumeroCuenta}\\clave.txt");// Ruta completa del archivo de clave
+            ArchivoMovimientos = Path.Combine($"C:\\Users\\hecto\\Downloads\\Cajero2.0\\\\usuario{NumeroCuenta}\\movimientos.txt");
+
+            movimientos = new List<string>();   //Con este comando esta inicializando la lista
+
+            if (!Directory.Exists(ArchivoCuenta))// Si el directorio no existe, lo crea
+                Directory.CreateDirectory(ArchivoCuenta);
+
+            if (!File.Exists(ArchivoSaldo))//Si el archivo de saldo no existe,lo crea
+                File.WriteAllText(ArchivoSaldo, "0");
+            Saldo = double.Parse(File.ReadAllText(ArchivoSaldo)); //Lee el saldo del archivo y lo asigna a la variable Saldo
+
+            if (!File.Exists(ArchivoClave))// Si el archivo de la clave no existe, lo crea (el "1234" es la clave por defecto)
+                File.WriteAllText(ArchivoClave, "1234");
+            Clave = File.ReadAllText(ArchivoClave); //Lee la clave del archivo y la asigna a la variable Clave
+
+            if (!File.Exists(ArchivoMovimientos))
+                File.Create(ArchivoMovimientos).Close();
+
+            Saldo = double.Parse(File.ReadAllText(ArchivoSaldo)); //Lee el saldo del archivo y lo asigna a la variable Saldo
+
+            string[] lineas = File.ReadAllLines(ArchivoMovimientos);
+            movimientos.AddRange(lineas);
+        }
+        public bool VerificarClave(string claveIngresada)
+        {
+            return Clave == claveIngresada; //Compara la clave ingresada con la clave almacenada
+
+        }
+        public void CambiarClave(string ClaveActual, string NuevaClave)
+        {
+            if (ClaveActual != Clave) //Verifica si la clave actual es correcta
+            {
+                Console.WriteLine("Clave actual incorrecta");
+                return;
+            }
+            Clave = NuevaClave; //Asigna la nueva clave a la variable Clave
+            File.WriteAllText(ArchivoClave, NuevaClave); //Escribe la nueva clave en el archivo
+            Console.WriteLine("Clave cambiada con exito");
+            RegistrarMovimiento($"Cambio de clave | {DateTime.Now}");
+            Console.ReadKey();
+
+        }
+        public void Depositar(double Monto)
+        {
+            Saldo += Monto; //Suma el monto al saldo
+            RegistrarMovimiento($"Depósito: {Monto} | {DateTime.Now}");
+            Console.WriteLine("Deposito realizado con exito");
+            Console.ReadKey();
+            Console.WriteLine($"Se ha depositado {Monto}. Nuevo saldo: {Saldo}");
+            GuardarSaldo(); //Guarda el saldo en el archivo
+            Console.ReadKey();
+        }
+        public void Retirar(double Monto)//Este metodo retira dinero de la cuenta
+        {
+            if (Monto <= Saldo)
+            {
+                Saldo -= Monto; //Resta el monto al saldo
+                RegistrarMovimiento($"Retiro: {Monto} | {DateTime.Now}");
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente para realizar el retiro.");
+            }
+            Console.WriteLine("Retiro realizado con exito");
+            Console.ReadKey();
+            Console.WriteLine($"Se ha retirado {Monto}. Nuevo saldo: {Saldo}");//Muestra el nuevo saldo
+            GuardarSaldo(); //Guarda el saldo en el archivo
+        }
+        public void ConsultarSaldo()
+        {
+            Console.WriteLine($"Su saldo actual es: {Saldo}");//Muestra el saldo
+            RegistrarMovimiento($"Consulta de saldo: {Saldo} | {DateTime.Now}");//Registra el movimiento
+            Console.ReadKey();
+        }
+        public void ConsultarMovimientos()
+        {
+            Console.WriteLine("=====ULTIMOS MOVIMIENTOS=====");
+            foreach (var movimiento in movimientos)
+            {
+                Console.WriteLine(movimiento);//Muestra cada movimiento
+            }
+            Console.ReadKey();
+        }
+        private void RegistrarMovimiento(string movimiento)
+        {
+            string Registro = $"cuenta {NumeroCuenta}: {movimiento} | {DateTime.Now}";
+            movimientos.Add(Registro); //Agrega el movimiento a la lista
+            File.AppendAllText(ArchivoMovimientos, Registro + Environment.NewLine); //Escribe el movimiento en el archivo
+        }
+        private void GuardarSaldo()
+        {
+            File.WriteAllText(ArchivoSaldo, Saldo.ToString()); //Escribe el saldo en el archivo
+        }
+
+    }
+}
---
+++ Cajero2.0/Program.cs
@@ -0,0 +1,94 @@
+﻿using Cajero;
+
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese su numero de cuenta:");
+        string numeroCuenta = Console.ReadLine();
+
+        CuentaBancaria Cuenta = new CuentaBancaria(numeroCuenta); // Crea una instancia de CuentaBancaria
+
+        //Comenzaremos con el proceso de autenticacion
+
+        int intentos = 0;
+        bool acceso = false;
+        while (intentos < 3 && !acceso)
+        {
+            Console.WriteLine("Ingrese su clave:");
+            string claveIngresada = Console.ReadLine();
+            if (Cuenta.VerificarClave(claveIngresada))
+            {
+                acceso = true;
+                Console.WriteLine("Acceso concedido. Bienvenido a su cuenta.");
+                Console.ReadKey();
+            }
+            else
+            {
+                intentos++;
+                Console.WriteLine("Clave incorrecta. Intentos restantes: " + (3 - intentos));
+                Console.ReadKey();
+            }
+        }
+
+        int opcion = 0;
+        do
+        {
+            if (!acceso)
+            {
+                Console.WriteLine("Demasiados intentos fallidos. Saliendo del sistema.");
+                break;
+            }
+            Console.WriteLine("____________________________________");
+            Console.WriteLine("|       == MENÚ PRINCIPAL ==       |");
+            Console.WriteLine("|----------------------------------|");
+            Console.WriteLine("|1.         Depositar              |");
+            Console.WriteLine("|__________________________________|");
+            Console.WriteLine("|2.          Retirar               |");
+            Console.WriteLine("|__________________________________|");
+            Console.WriteLine("|3.       Consultar Saldo          |");
+            Console.WriteLine("|__________________________________|");
+            Console.WriteLine("|4.  Consultar ultimos movimientos |");
+            Console.WriteLine("|__________________________________|");
+            Console.WriteLine("|5.        Cambiar Clave           |");
+            Console.WriteLine("|__________________________________|");
+            Console.WriteLine("|6.            Salir               |");
+            Console.WriteLine("|__________________________________|");
+
+            opcion = int.Parse(Console.ReadLine());
+            switch (opcion)
+            {
+                case 1:
+                    Console.WriteLine("Ingrese el monto a depositar:");
+                    double MontoDeposito = double.Parse(Console.ReadLine());
+                    Cuenta.Depositar(MontoDeposito);
+                    break;
+                case 2:
+                    Console.WriteLine("Ingrese el monto a retirar:");
+                    double MontoRetiro = double.Parse(Console.ReadLine());
+                    Cuenta.Retirar(MontoRetiro);
+                    break;
+                case 3:
+                    Cuenta.ConsultarSaldo();
+                    break;
+                case 4:
+                    Cuenta.ConsultarMovimientos();
+                    break;
+                case 5:
+                    Console.WriteLine("Ingrese su clave actual:");
+                    string ClaveActual = Console.ReadLine();
+                    Console.WriteLine("Ingrese su nueva clave:");
+                    string NuevaClave = Console.ReadLine();
+                    Cuenta.CambiarClave(ClaveActual, NuevaClave);
+                    break;
+                case 6:
+                    Console.WriteLine("Gracias por usar el cajero. ¡Hasta luego!");
+                    break;
+                default:
+                    Console.WriteLine("Opción no válida. Intente de nuevo.");
+                    break;
+            }
+        } while (opcion != 6 && acceso);
+    }
+}
