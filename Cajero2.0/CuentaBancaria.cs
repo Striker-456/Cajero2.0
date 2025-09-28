89f818d3b760241c6cb3103a05c65da1c5840bd9
Hector Velasquez <hectordv28@gmail.com>
2025-09-28 17:02:18 -0500

    Elimine el "DateTime.now" del metodo de ConsultarSaldo para evitar que se repita la fecha en los registro de los movimientos y tambien modificaciones en la asignacion de atributos y variables co nombre "movimiento".

--- Cajero2.0/CuentaBancaria.cs
+++ Cajero2.0/CuentaBancaria.cs
@@ -18,7 +18,7 @@ namespace Cajero
         private string Clave;// La clave es privada para que no pueda ser accedida desde fuera de la clase
         public double Saldo { get; private set; }// El saldo es publico para que pueda ser accedido desde fuera de la clase pero solo puede ser modificado desde dentro de la clase
 
-        private List<string> movimientos;
+        private List<string> Movimientos;
         private string ArchivoMovimientos;
         private string ArchivoClave;
         private string ArchivoSaldo;
@@ -33,7 +33,7 @@ namespace Cajero
             ArchivoClave =$"C:\\Users\\hecto\\Downloads\\Cajero2.0\\usuario{NumeroCuenta}\\clave.txt";// Ruta completa del archivo de clave
             ArchivoMovimientos = $"C:\\Users\\hecto\\Downloads\\Cajero2.0\\\\usuario{NumeroCuenta}\\movimientos.txt";
 
-            movimientos = new List<string>();   //Con este comando esta inicializando la lista
+            Movimientos = new List<string>();   //Con este comando esta inicializando la lista
 
             if (!Directory.Exists(ArchivoCuenta))// Si el directorio no existe, lo crea
                 Directory.CreateDirectory(ArchivoCuenta);
@@ -52,7 +52,7 @@ namespace Cajero
             Saldo = double.Parse(File.ReadAllText(ArchivoSaldo)); //Lee el saldo del archivo y lo asigna a la variable Saldo
 
             string[] lineas = File.ReadAllLines(ArchivoMovimientos);//Lee todas las lineas del archivo de movimientos y las asigna a un array
-            movimientos.AddRange(lineas);
+            Movimientos.AddRange(lineas);
         }
         public bool VerificarClave(string claveIngresada)//Este metodo verifica si la clave ingresada es correcta
         {
@@ -88,7 +88,7 @@ namespace Cajero
             if (Monto <= Saldo)//Verifica si hay saldo suficiente
             {
                 Saldo -= Monto; //Resta el monto al saldo
-                RegistrarMovimiento($"Retiro: {Monto} | {DateTime.Now}");//Registra el movimiento
+                RegistrarMovimiento($"Retiro: {Monto}");//Registra el movimiento
             }
             else
             {
@@ -102,13 +102,13 @@ namespace Cajero
         public void ConsultarSaldo()//Este metodo consulta el saldo de la cuenta
         {
             Console.WriteLine($"Su saldo actual es: {Saldo}");//Muestra el saldo
-            RegistrarMovimiento($"Consulta de saldo: {Saldo} | {DateTime.Now}");//Registra el movimiento
+            RegistrarMovimiento($"Consulta de saldo: {Saldo}");//Registra el movimiento
             Console.ReadKey();//Espera a que el usuario presione una tecla
         }
         public void ConsultarMovimientos()//Este metodo consulta los ultimos movimientos de la cuenta
         {
             Console.WriteLine("\n=====ULTIMOS MOVIMIENTOS=====");//Muestra el titulo
-            foreach (var Movimiento in movimientos)//Recorre la lista de movimientos
+            foreach (var Movimiento in Movimientos)//Recorre la lista de movimientos
             {
                 Console.WriteLine(Movimiento);//Muestra cada movimiento
             }
@@ -117,7 +117,7 @@ namespace Cajero
         private void RegistrarMovimiento(string Movimiento)//Este metodo registra un movimiento en la lista y en el archivo
         {
             string Registro = $"cuenta {NumeroCuenta}: {Movimiento} | {DateTime.Now}";//Crea el registro del movimiento
-            movimientos.Add(Registro); //Agrega el movimiento a la lista
+            Movimientos.Add(Registro); //Agrega el movimiento a la lista
             File.AppendAllText(ArchivoMovimientos, Registro + Environment.NewLine); //Escribe el movimiento en el archivo
         }
         private void GuardarSaldo()//Este metodo guarda el saldo en el archivo
